3800b9ba090324f0a4f38db2c3a1245051189709
oherna25 <89622466+oherna25@users.noreply.github.com>
2021-10-25 07:48:49 -0500

    started

--- src/main/java/JavaFXTemplate.java
+++ src/main/java/JavaFXTemplate.java
@@ -1,14 +1,18 @@
 import javafx.application.Application;
-
+import javafx.scene.Group;
 import javafx.scene.Scene;
-
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class JavaFXTemplate extends Application {
 	
-	
-
+	Scene startup_screen ;
+	Group root;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		launch(args);
@@ -19,9 +23,27 @@ public class JavaFXTemplate extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
 		primaryStage.setTitle("Welcome to Connect Four!");
-		Group root
-		Scene scene = new Scene(root, 700,700);
-		primaryStage.setScene(scene);
+		root = new Group();
+		MenuBar menuBar = new MenuBar();
+		Menu gamePlay = new Menu("Game Play");
+		menuBar.getMenus().add(gamePlay);
+		MenuItem reverse = new MenuItem("Reverse reverse");
+		gamePlay.getItems().add(reverse);
+		
+		VBox menu = new VBox(menuBar);
+		
+		
+		Label label1 = new Label("Welcome screen");
+		Button start = new Button("start game");
+		VBox screen1 = new VBox(20);
+		screen1.getChildren().addAll(label1, start);
+		startup_screen = new Scene(menu, 700, 700);
+		
+		
+		VBox main_screen = new VBox(20);
+		
+		Scene main = new Scene(main_screen, 700,700);
+		primaryStage.setScene(startup_screen);
 		primaryStage.show();
 	}
 
--- src/main/java/sceneSetter.java
+++ src/main/java/sceneSetter.java
@@ -1,5 +1,6 @@
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 //import javafx.scene.control.Button;
 
 
@@ -9,6 +10,20 @@ public class sceneSetter extends Scene {
 		super(root, width, height);
 		// TODO Auto-generated constructor stub
 	}
+	
+	public void setBG(Color color) {
+		
+		this.setFill(color);
+	}
+	
+	public void gmImage (String fileName) {
+		
+	}
+	
+	
+	
+	
+	
 
 
 	
