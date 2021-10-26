2e21f7821d4ce1b6c4789a1390040895491414af
oherna25 <89622466+oherna25@users.noreply.github.com>
2021-10-25 21:56:10 -0500

    Revert "Revert "close""
    This reverts commit 42654034159f78c73dc1a558f9d1012403654f74.

--- src/main/java/gameButton.java
+++ src/main/java/gameButton.java
@@ -1,3 +1,5 @@
+import com.sun.prism.paint.Color;
+
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderStrokeStyle;
 
@@ -5,14 +7,16 @@ public class gameButton extends Button {
 
 	public gameButton() {
 		// TODO Auto-generated constructor stub
-		this.minHeight(500);
-		this.minWidth(500);
+		this.minHeight(200);
+		this.minWidth(200);
 		//this.setBackground(null);
 		this.setStyle("-fx-border-width: 1; -fx-border-color: black;");
 
 		this.setBackground(null);
 		this.setText("-");
 	}
+
+
 	
 
 
